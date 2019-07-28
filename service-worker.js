a2f08a741d71547b5fce312ed0d2408d4e56484a
Robbie Carlton <robbie.carlton@gmail.com>
2019-07-28 15:06:08 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robbiecarlton/react-identicon-variety-pack/precache-manifest.213b236e447a449f7fe073581e23b127.js"
+  "/precache-manifest.f9abe1d8c6639a18aa4d852b1ddc21f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robbiecarlton/react-identicon-variety-pack/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
